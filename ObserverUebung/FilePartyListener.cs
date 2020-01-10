2c7ba18e1fe92a190eba0ca2983c9009079516e6
Leonard Schubert <leonards4@schule.koeln>
2020-01-10 14:32:29 +0100

    initial

---
+++ ObserverUebung/FilePartyListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverUebung 
+{
+    class FilePartyListener : AbstractFileAddedListener
+    {
+        public override void NotifyFileAdded(string file)
+        {
+            Console.WriteLine("PARTY");
+        }
+    }
+}
--- ObserverUebung/Program.cs
+++ ObserverUebung/Program.cs
@@ -18,10 +18,15 @@ namespace ObserverUebung
             Logger logger2 = new Logger("C:\\Users\\lschubert\\Documents\\report.txt");
             folderWatch.FileAddedListeners.Add(new FileAddedListener(logger1));
             folderWatch.FileAddedListeners.Add(new FileAddedListener(logger2));
-
+            FilePartyListener filePartyListener = new FilePartyListener();
+            folderWatch.FileAddedListeners.Add(filePartyListener);
 
 
             //Test des Programms.
+            //Der "hinzugefügte" File wird auf der Konsole angegeben
+            //Ein tatsächlicher File-Name ist nicht notwendig
+            //Beispiel-Eingabe: "abc"
+            //Die Eingabe soll auf der Konsole und im Logfile ausgegeben werden.
             while(true)
             {
                 string file = Console.ReadLine();
